d9f487e050a9d79232e6e5e98a702fcc66c23744
christianmacasinagrodriguez <rodriguezmacasinagchristian@gmail.com>
2023-07-26 21:17:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Job Portal Multi-page Static Front-end Project
+
+Capstone Project
+
+## Technologies
+
+- EJS
+- Node.js
+- Express
+- Express EJS Layout
+- Tailwind
+
+## How to run
+
+```
+npm install
+```
+
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+
+})
+
+app.get('/about', (req, res) => {
+    res.render('pages/about')
+
+})
+
+app.listen(3000, ()=> {
+    console.log('listening on port 3000')
+})
