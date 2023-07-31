bc17201774ad95ac4526e90ef7b75a6a49ea21ee
ErickaConcepcion <eriorion001@gmail.com>
2023-07-31 21:02:12 +0800

    new folder

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ const app = express()
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('pages/index')
 
-})
-
-app.get('/about', (req, res) => {
-    res.render('pages/about')
-
-})
 
 app.listen(3000, ()=> {
     console.log('listening on port 3000')
