c722df750a05467c7b29f1d1975e44fd39ace4df
christianmacasinagrodriguez <rodriguezmacasinagchristian@gmail.com>
2023-08-01 20:03:51 +0800

    Created the adventure category section and made the callback section responsive to all screen sizes

---
+++ public/assets/data/adventureCategories.js
@@ -0,0 +1,34 @@
+const adventureCategories = [
+    {
+        adventureIcon: 'https://demo.bosathemes.com/html/travele/assets/images/icon6.png',
+        adventureTitle: 'Adventure',
+        adventureDestinations: 15
+    },
+    {
+        adventureIcon: 'https://demo.bosathemes.com/html/travele/assets/images/icon10.png',
+        adventureTitle: 'Trekking',
+        adventureDestinations: 12
+    },
+    {
+        adventureIcon: 'https://demo.bosathemes.com/html/travele/assets/images/icon9.png',
+        adventureTitle: 'Camp Fire',
+        adventureDestinations: 7
+    },
+    {
+        adventureIcon: 'https://demo.bosathemes.com/html/travele/assets/images/icon8.png',
+        adventureTitle: 'Off Road',
+        adventureDestinations: 15
+    },
+    {
+        adventureIcon: 'https://demo.bosathemes.com/html/travele/assets/images/icon7.png',
+        adventureTitle: 'Camping',
+        adventureDestinations: 13
+    },
+    {
+        adventureIcon: 'https://demo.bosathemes.com/html/travele/assets/images/icon11.png',
+        adventureTitle: 'Exploring',
+        adventureDestinations: 25
+    }
+]
+
+module.exports = adventureCategories
