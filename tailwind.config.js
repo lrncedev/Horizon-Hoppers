5688073755897cbc3613a2ae3c10e83e5a88fcf6
ErickaConcepcion <eriorion001@gmail.com>
2023-08-02 22:27:34 +0800

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/**/*.ejs'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [ './views/**/*.ejs' ],
+	theme: {
+		extend: {
+			colors: {
+				'dodger-blue': '#0791be',
+				'watermelon-red': '#f56960', 
+				'midnight': '#101F46'
+			},
+			fontFamily: {
+				raleway: [ 'Raleway', 'sans-serif' ],
+				opensans: [ 'Open Sans', 'sans-serif' ]
+			}
+		}
+	},
+	plugins: []
+};
