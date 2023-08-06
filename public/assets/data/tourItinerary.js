bd1766813b5b8469fd4e4b2d6d5f850f3eaf5374
Laurence Cadag <laurencecads1@gmail.com>
2023-08-06 18:13:02 +0800

    tour-itinerary array

---
+++ public/assets/data/tourItinerary.js
@@ -0,0 +1,25 @@
+const itinerary = [
+  {
+    count: '01',
+    title: 'Day 1: Arrive South Africa Forest',
+    time: '8:00 am to 10:00 am',
+    description: 'Lorem ipsum available isn but the majority have suffered alteratin in some or form injected. Lorem ipsum is simply free text used by copytyping refreshing. Neque porro est qui dolorem ipsum quia quaed inventore veritatis et quasi dicta sunt explicabo.',
+    inclusion: ['Free Drinks', 'Awesome Breakfast', '5 Star Accommodation']
+  },
+  {
+    count: '02',
+    title: 'Day 2: Lunch Inside of Forest & Adventure',
+    time: '8:00 am to 10:00 am',
+    description: 'Lorem ipsum available isn but the majority have suffered alteratin in some or form injected. Lorem ipsum is simply free text used by copytyping refreshing. Neque porro est qui dolorem ipsum quia quaed inventore veritatis et quasi dicta sunt explicabo.',
+    inclusion: ['Free Drinks', 'Awesome Breakfast', '5 Star Accommodation']
+  },
+  {
+    count: '03',
+    title: 'Day 3: Depart from South Africa',
+    time: '8:00 am to 10:00 am',
+    description: 'Lorem ipsum available isn but the majority have suffered alteratin in some or form injected. Lorem ipsum is simply free text used by copytyping refreshing. Neque porro est qui dolorem ipsum quia quaed inventore veritatis et quasi dicta sunt explicabo.',
+    inclusion: ['Free Drinks', 'Awesome Breakfast', '5 Star Accommodation']
+  }
+]
+
+module.exports = itinerary
