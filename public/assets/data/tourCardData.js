147391e87d70c3944527d629ab90b8e13850f6a8
christianmacasinagrodriguez <140526893+christianmacasinagrodriguez@users.noreply.github.com>
2023-08-07 16:06:34 +0800

    Made the tour card dynamic

--- public/assets/data/tourCardData.js
+++ public/assets/data/tourCardData.js
@@ -5,7 +5,8 @@ const tourCardData = [
         tourCardTitle: 'Maldives Beach 2 Days Tour',
         tourCardDays: '3 Days',
         tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
+        tourCardDestination: 'Maldives',
+        tourCardWishList: false
 
     },
     {
@@ -14,7 +15,8 @@ const tourCardData = [
         tourCardTitle: 'Maldives Beach 2 Days Tour',
         tourCardDays: '3 Days',
         tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
+        tourCardDestination: 'Maldives',
+        tourCardWishList: true
 
     },
     {
@@ -23,7 +25,8 @@ const tourCardData = [
         tourCardTitle: 'Maldives Beach 2 Days Tour',
         tourCardDays: '3 Days',
         tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
+        tourCardDestination: 'Maldives',
+        tourCardWishList: false
 
     },
     {
@@ -32,7 +35,8 @@ const tourCardData = [
         tourCardTitle: 'Maldives Beach 2 Days Tour',
         tourCardDays: '3 Days',
         tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
+        tourCardDestination: 'Maldives',
+        tourCardWishList: true
 
     },
     {
@@ -41,7 +45,8 @@ const tourCardData = [
         tourCardTitle: 'Maldives Beach 2 Days Tour',
         tourCardDays: '3 Days',
         tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
+        tourCardDestination: 'Maldives',
+        tourCardWishList: false
 
     },
     {
@@ -50,7 +55,8 @@ const tourCardData = [
         tourCardTitle: 'Maldives Beach 2 Days Tour',
         tourCardDays: '3 Days',
         tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
+        tourCardDestination: 'Maldives',
+        tourCardWishList: false
 
     },
     {
@@ -59,7 +65,8 @@ const tourCardData = [
         tourCardTitle: 'Maldives Beach 2 Days Tour',
         tourCardDays: '3 Days',
         tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
+        tourCardDestination: 'Maldives',
+        tourCardWishList: false
 
     },
     {
@@ -68,16 +75,8 @@ const tourCardData = [
         tourCardTitle: 'Maldives Beach 2 Days Tour',
         tourCardDays: '3 Days',
         tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
-
-    },
-    {
-        tourCardImage: 'https://images.unsplash.com/photo-1516815231560-8f41ec531527?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1167&q=80',
-        tourCardRating: '8.0 Superb',
-        tourCardTitle: 'Maldives Beach 2 Days Tour',
-        tourCardDays: '3 Days',
-        tourCardPax: '12+',
-        tourCardDestination: 'Maldives'
+        tourCardDestination: 'Maldives',
+        tourCardWishList: true
 
     },
 ]
