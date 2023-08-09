cf16e02248ebb4a30e8874bd849e9ff84fc17499
Laurence Cadag <laurencecads1@gmail.com>
2023-08-09 23:36:04 +0800

    routes for details and tours

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const adventureCategories = require('./public/assets/data/adventureCategories')
 const offersData = require('./public/assets/data/offersData')
 const carouselData = require('./public/assets/data/carouselData')
 const itinerary = require('./public/assets/data/tourItinerary')
-const tourCardData = require('./public/assets/data/tourCardData')
 const places = require('./public/assets/data/destinationsData')
 const expressLayout = require('express-ejs-layouts')
 const app = express()
@@ -15,15 +14,23 @@ app.set('view engine', 'ejs')
 app.set('layout', './layouts/master-layout.ejs')
 
 app.get('/', (req, res)=> {
-    res.render('pages/landing-page.ejs', {adventureCategories, offersData, carouselData})
+  res.render('pages/landing-page.ejs', {adventureCategories, offersData, carouselData, places})
 })
 
-app.get('/tours', (req, res)=> {
-    res.render('pages/tours.ejs', {itinerary, tourCardData})
+app.get('/tours', (req, res) => {
+  let placeholder = [];
+
+  places.forEach((place) => {
+    place.tours.forEach((specific) => {
+      placeholder.push(specific)
+    })
+  })
+
+  res.render('pages/tours.ejs', {itinerary, tourCardData: placeholder})
 })
 
 app.get('/destination', (req, res)=> {
-    res.render('pages/destination.ejs')
+  res.render('pages/destination.ejs', {destinations: places})
 })
 
 app.get('/tours/:place', (req, res) => {
@@ -34,15 +41,26 @@ app.get('/tours/:place', (req, res) => {
     return res.status(404).send('Place not found');
   }
 
-  res.render('pages/tours.ejs', {itinerary: place.itinerary, tourCardData : place.tours});
+  res.render('pages/tours.ejs', {itinerary: place.itinerary, tourCardData: place.tours});
 })
 
 app.get('/booking', (req, res)=> {
     res.render('pages/booking.ejs')
 })
 
-app.get('/details', (req, res)=> {
-    res.render('pages/tour-details.ejs')
+app.get('/details/:place/:id', (req, res) => {
+  const requestedId = req.params.id;
+  const requestedPlace = req.params.place;
+
+  const place = places.find(item => item.country.toLowerCase() === requestedPlace.toLowerCase());
+
+  const tour = place.tours.find(tour => tour.id === Number(requestedId));
+
+  if (!place) {
+    return res.status(404).send('Place not found');
+  }
+
+  res.render('pages/tour-details.ejs', {tour})
 })
 
 
