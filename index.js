446179e00c46948b6ef66cf14db85fd25a259eb9
Laurence Cadag <laurencecads1@gmail.com>
2023-08-09 12:08:05 +0800

    route for specific destinations tour

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const offersData = require('./public/assets/data/offersData')
 const carouselData = require('./public/assets/data/carouselData')
 const itinerary = require('./public/assets/data/tourItinerary')
 const tourCardData = require('./public/assets/data/tourCardData')
+const places = require('./public/assets/data/destinationsData')
 const expressLayout = require('express-ejs-layouts')
 const app = express()
 
@@ -24,6 +25,18 @@ app.get('/tours', (req, res)=> {
 app.get('/destination', (req, res)=> {
     res.render('pages/destination.ejs')
 })
+
+app.get('/tours/:place', (req, res) => {
+  const requestedPlace = req.params.place;
+  const place = places.find(item => item.country.toLowerCase() === requestedPlace.toLowerCase());
+
+  if (!place) {
+    return res.status(404).send('Place not found');
+  }
+
+  res.render('pages/tours.ejs', {itinerary: place.itinerary, tourCardData : place.tours});
+})
+
 app.get('/booking', (req, res)=> {
     res.render('pages/booking.ejs')
 })
